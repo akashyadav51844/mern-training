4d5ce2c776320624ac68e2132f36db5e95236235
akash yadav <akashyadav51844@gmail.com>
2026-06-29 23:26:43 +0530

    Add files via upload

---
+++ facebook.js
@@ -0,0 +1,31 @@
+function showData() {
+
+let fname = document.getElementById("fname").value;
+
+let lname = document.getElementById("lname").value;
+
+let password =document.getElementById("password").value;
+
+let day  = document.getElementById("day").value;
+
+let month= document.getElementById("month").value;
+
+let year= document.getElementById("year").value;
+
+let gender= document.getElementById("gender").value;
+
+let email= document.getElementById("email").value;
+
+
+
+console.log(`First name: ${fname}`);
+console.log(`last name: ${lname}`);
+console.log(`password: ${password}`);
+console.log(`day: ${day}`);
+console.log(`month : ${month}`);
+console.log(`year: ${year}`);
+console.log(`email: ${email}`);
+
+
+
+}
